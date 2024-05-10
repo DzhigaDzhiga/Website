ef0f3fb8dfd7a9d37978458c9704efb2b57c7683
DzhigaDzhiga <144116592+DzhigaDzhiga@users.noreply.github.com>
2024-05-10 13:52:22 +0700

    Add files via upload

---
+++ travel/tpl/footer.php
@@ -0,0 +1,8 @@
+        <footer class="footer">
+            <div class="row">
+                <div class="col">
+                    Это сайт, предназначенный для обучения
+                </div>
+            </div>
+        </footer>
+    </div>
